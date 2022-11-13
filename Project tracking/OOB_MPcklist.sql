5d5675c1dd7496f682ae3e70504e2aaaec38b4b6
CPWRF <cooper.wang@garmin.com>
2022-11-14 07:52:55 +0800

    updated

--- Project tracking/OOB_MPcklist.sql
+++ Project tracking/OOB_MPcklist.sql
@@ -1,14 +1,13 @@
-SELECT [PROJECT_NAME]
-      ,[UpdateTime]
-      /*
-      ,[T_MPC_CkListInfo].[MP_ID]
-      ,[T_MPC_CkListInfo].[Item_ID]
-      */
+SELECT [T_MPC_CkListInfo].[PROJECT_SIZE_ID]
+      ,[PROJECT_NAME]
+      /*,[T_MPC_CkListInfo].[MP_ID]
+      ,[T_MPC_CkListInfo].[Item_ID]*/
       ,[Item_Name] as 'Description'
       ,[ORG_FILE_NAME] as 'Uploaded file name'
-      ,[Status] /* If item is checked, 1=checked, 2=N/A */
       ,[Comment] /* User comment */
+      ,[Status] /* If item is checked, 1=checked, 2=N/A */
       ,[UpdateUser]
+      ,[UpdateTime]
   FROM [PR_WEB2].[dbo].[T_MPC_CkListInfo]
 
   LEFT JOIN [dbo].[T_PS_ProdSizeRelateInfo] 
@@ -22,7 +21,5 @@ SELECT [PROJECT_NAME]
 
   where Item_Name like 'OOB%'
   /* Item_Name is 'Descroption' in MP information */
-
-  order by [UpdateTime] DESC
-
-  
+ 
+  ORDER BY [UpdateTime] DESC
