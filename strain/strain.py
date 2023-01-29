581e418f86b96c0b74b7fff2292a1ae25fedbf03
CPWRF <cooper.wang@garmin.com>
2023-01-29 10:33:10 +0800

    update

--- strain/strain.py
+++ strain/strain.py
@@ -1,9 +1,9 @@
-import pyodbc
+#import pyodbc
 import pandas as pd
 from os import getcwd
 from sqlalchemy.engine import URL
 from sqlalchemy import create_engine
-from UliPlot.XLSX import auto_adjust_xlsx_column_width
+#from UliPlot.XLSX import auto_adjust_xlsx_column_width
 
 gpn = input ("Enter whole 105-GPN (according to SMTVip) :")
 
@@ -32,14 +32,21 @@ connection_url = URL.create("mssql+pyodbc", query={"odbc_connect": connection_st
 engine = create_engine(connection_url)
 raw = pd.read_sql(strain_query, engine)
 
-raw.judge = raw.judge.map({'OK':1, 'NG':0})
-raw.sort_values('createdata', ascending=False, inplace=True)
-
-droped = raw.drop_duplicates(subset=['PCBver','station','Position','memo','org'])
-pivot = droped.groupby(['station','memo','PCBver','createdata','Position'])['judge'].sum().unstack('Position').sort_index(level=['station','createdata'],ascending=False).style.highlight_min(color='red', axis=None)
+def pivot_raw(raw):
+    return(raw
+    .assign(judge = raw.judge.map({'OK':1, 'NG':0}).astype('int8'))
+    .sort_values('createdata', ascending=False)
+    .drop_duplicates(subset=['PCBver','station','Position','memo'])
+    .groupby(['station','memo','PCBver','createdata','Position'])
+    ['judge']
+    .sum()
+    .unstack('Position')
+    .sort_index(level=['station','createdata'],ascending=False)
+    .style.applymap(lambda val : "background-color: red" if val == 0 else "")
+    )
 
 with pd.ExcelWriter(getcwd()+'\{}_StrainResult.xlsx'.format(gpn), engine='openpyxl') as writer:
-    pivot.to_excel(writer, sheet_name='pivot')
+    pivot_raw(raw).to_excel(writer, sheet_name='pivot')
 
-    raw.to_excel(writer, sheet_name='raw')
-    auto_adjust_xlsx_column_width(raw, writer, sheet_name="raw", margin=3)
+    raw.to_excel(writer, sheet_name='raw', index=False)
+    #105-04263-20auto_adjust_xlsx_column_width(raw, writer, sheet_name="raw", margin=3)
